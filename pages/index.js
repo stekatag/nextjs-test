c66a764dbb4ecb71b82f31c3903736fc3d320f25
Stefan Gogov <stefangogov@proton.me>
2024-04-24 16:57:14 +0300

    Update hero image in index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { AgencySelection, Hero } from "../sections";
 
 const heroProps = {
-  image: { src: "/img/heroimg.png", alt: "", width: 1000, height: 1680 },
+  image: { src: "/img/heroimg.jpg", alt: "", width: 1000, height: 1680 },
   title: "Agency procurement, outsourced.",
   description: "Start the process here",
   ctaText: "Start",
